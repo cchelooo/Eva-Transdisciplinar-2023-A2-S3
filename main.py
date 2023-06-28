8b87accae34ea0588fa8904e3702b728ff6b7dd9
cchelo <132941573+cchelooo@users.noreply.github.com>
2023-06-28 13:35:42 -0400

    Update Main Final
    Codigo completo
    
    Co-Authored-By: Nelson Neculhueque <134446391+NelsonNeculhueque@users.noreply.github.com>
    Co-Authored-By: Yaninna Alvarez <134446812+Yaninna137@users.noreply.github.com>
    Co-Authored-By: Eduardo Escares <129316732+eduardoscrs@users.noreply.github.com>

--- main.py
+++ main.py
@@ -3,84 +3,96 @@ from tkinter import *
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from PIL import Image, ImageTk
 
+# ANIMACION VELOCIDAD
 def animacion(dato):
     global background_photo, car_photo        
-    canvas = tk.Canvas(ventana, width=700, height=146)
+    canvas = tk.Canvas(ventana, width=700, height=146)  # Canvas para poder "dibujar" en ella
     canvas.place(x=386, y=5)
 
-    background_image = Image.open("img/road3.png")
-    background_photo = ImageTk.PhotoImage(background_image)
-    background1 = canvas.create_image(0, 0, image=background_photo, anchor="nw")
-    background2 = canvas.create_image(700, 0, image=background_photo, anchor="nw")
+    background_image = Image.open("img/road3.png")  # Importar imagen del fondo
+    background_photo = ImageTk.PhotoImage(background_image)   # Guardar imagen en variable
+    background1 = canvas.create_image(0, 0, image=background_photo, anchor="nw")    # Crear imagen en pos 0, 0
+    background2 = canvas.create_image(700, 0, image=background_photo, anchor="nw")  # Crear imagen 2 en pos 700, 0
 
-    car_image = Image.open("img/car.png")
-    car_image = car_image.resize((80, 100))
-    car_photo = ImageTk.PhotoImage(car_image)
-    car = canvas.create_image(300, 120, image=car_photo)
+    car_image = Image.open("img/car.png")   # Importar imagen del auto
+    car_image = car_image.resize((80, 100)) # Cambiar tamaño de la imagen
+    car_photo = ImageTk.PhotoImage(car_image) # Guardar imagen en variable
+    car = canvas.create_image(300, 120, image=car_photo)    # Crear imagen del auto en pos 300, 120
 
+    #FUNCION PARA MOVER EL FONDO
     def move_background():
-        if dato == 'pos':
-            canvas.move(background1, -v, 0)
-            canvas.move(background2, -v, 0)
-        elif dato == 'vel':
-            canvas.move(background1, -div, 0)
-            canvas.move(background2, -div, 0)
-        elif dato == 'tie':
-            canvas.move(background1, -v, 0)
-            canvas.move(background2, -v, 0)
-        else:
-            canvas.move(background1, 0, 0)
+        if dato == 'pos':                           # Validar selección de boton y sus datos
+            canvas.move(background1, -v, 0) # Mover fondo solo en x, a la velocidad elegida.
+            canvas.move(background2, -v, 0) # Mover fondo 2 solo en x, a la velocidad elegida.
+        elif dato == 'vel':                         # Validar selección de boton y sus datos
+            canvas.move(background1, -div, 0) # Mover fondo solo en x, a la velocidad elegida.
+            canvas.move(background2, -div, 0) # Mover fondo 2 solo en x, a la velocidad elegida.
+        elif dato == 'tie':                         # Validar selección de boton y sus datos
+            canvas.move(background1, -v, 0) # Mover fondo solo en x, a la velocidad elegida.
+            canvas.move(background2, -v, 0) # Mover fondo 2 solo en x, a la velocidad elegida.
+        else:                                       # Estatico
+            canvas.move(background1, 0, 0) 
             canvas.move(background2, 0, 0)
+            
+        # VALIDAR LAS COORDENADAS DE LOS FONDOS PARA REPETIR EN BUCLE
         if canvas.coords(background1)[0] < -700:
             canvas.move(background1, 1400, 0)
         if canvas.coords(background2)[0] < -700:
             canvas.move(background2, 1400, 0)
         canvas.after(60, move_background)
-    move_background() 
+
+    move_background() # Llamar a la funcion
+
+# TEXTOS DE LA ANIMACION DE VELOCIDAD
 def velo_animacion(dato):
+    # TEXTOS DE CALCULOS                                                                                                                                
     if dato == 'pos':
-        vel_anima.configure(text=f'  v = Δx / Δt\n  v = ({posi} - {0}) / ({t} - {0})\n  v = {v}m/s', font=("Bonodi", 10, "bold italic"), justify=LEFT, anchor='w')
+        vel_anima.configure(text=f'  v = Δx / Δt\n  v = ({posi} - {0}) / ({t} - {0})\n  v = {v}m/s', font=("Bonodi", 10, "bold italic"), bg='#DFDDC7', justify=LEFT, anchor='w') # Cambiar texto a nuevos datos
     elif dato == 'vel':
-        vel_anima.configure(text=f'  v = Δx / Δt\n  v = ({xF} - {xI}) / ({tF} - {tI})\n  v = {div}m/s', font=("Bonodi", 10, "bold italic"), justify=LEFT, anchor='w')
+        vel_anima.configure(text=f'  v = Δx / Δt\n  v = ({xF} - {xI}) / ({tF} - {tI})\n  v = {div}m/s', font=("Bonodi", 10, "bold italic"), bg='#DFDDC7', justify=LEFT, anchor='w') # Cambiar texto a nuevos datos
     elif dato == 'tie':
-        vel_anima.configure(text=f'  v = Δx / Δt\n  v = ({x} - {0}) / ({tiem} - {0})\n  v = {v}m/s', font=("Bonodi", 10, "bold italic"), justify=LEFT, anchor='w')
+        vel_anima.configure(text=f'  v = Δx / Δt\n  v = ({x} - {0}) / ({tiem} - {0})\n  v = {v}m/s', font=("Bonodi", 10, "bold italic"), bg='#DFDDC7', justify=LEFT, anchor='w') # Cambiar texto a nuevos datos
+# FONDO DE LOS BOTONES
 def fondo_btn(dato):
     global total
-    btn_body = Frame(ventana, bg='#FFEBC9')
+    btn_body = Frame(ventana, bg='#FFEBC9') # Fondo de los botones
     btn_body.place(x=25, y=365, width=326, height=305)
 
-    btn_header = Label(ventana, text='Ingrese los siguientes datos: ', bg='#211717', fg='#fff')
+    btn_header = Label(ventana, text='Ingrese los siguientes datos: ', bg='#211717', fg='#fff') # Header de los botones
     btn_header.place(x=25, y=365, width=326)
 
-    total = Label(ventana, text='', bg='#FFEBC9')
+    total = Label(ventana, text='', bg='#FFEBC9')   # Texto de los datos.
     total.place(x=93, y=547)
-    animacion('')
+    animacion('')   # Cargar funcion animacion para que este estatica
     vel_anima.configure(text='INGRESA DATOS', font='TkDefaultFont', justify=CENTER, anchor='center')
     if dato == "posicion":
-        enviar_btn = Button(ventana, text='Guardar', bg='#DFDDC7', command=pos)
+        enviar_btn = Button(ventana, text='Guardar', bg='#DFDDC7', command=calculo_pos) # Boton enviar POSICION
     elif dato == "velocidad":
-        enviar_btn = Button(ventana, text='Guardar', bg='#DFDDC7', command=vel)
+        enviar_btn = Button(ventana, text='Guardar', bg='#DFDDC7', command=calculo_vel) # Boton enviar VELOCIDAD
     elif dato == "tiempo":
-        enviar_btn = Button(ventana, text='Guardar', bg='#DFDDC7', command=tie)
+        enviar_btn = Button(ventana, text='Guardar', bg='#DFDDC7', command=calculo_tie) # Boton enviar TIEMPO
     enviar_btn.place(x=138, y=497, width=100)
 
-def posicion():
-    global v_box, t_box, seleccion
-    fondo_btn("posicion")
+# INGRESAR DATOS POSICION
+def entry_posicion():
+    global v_box, t_box
+    fondo_btn("posicion") # Iniciar
     definicion.configure(text=var_mru[0]['defi_posicion'])
     v = Label(ventana, text= "Ingrese velocidad en m/s", fg="#000", bg="#FFEBC9")
     t = Label(ventana, text= "Ingrese tiempo en s:", fg="#000", bg="#FFEBC9")   
     v.place(x=44, y=391)
     t.place(x=44, y=412)
 
-    v_box = Entry(ventana)
+    # validacion de texto
+    v_box = Entry(ventana, validate='key', validatecommand=(validacion, '%P'))
     v_box.place(x=208, y=391)
-    t_box = Entry(ventana)
+    t_box = Entry(ventana, validate='key', validatecommand=(validacion, '%P'))
     t_box.place(x=208, y=412)
     ventana.mainloop()
 
-def velocidad():
-    global xI_box, xF_box, tI_box, tF_box, seleccion
+# INGRESAR DATOS POSICION
+def entry_velocidad():
+    global xI_box, xF_box, tI_box, tF_box
     fondo_btn("velocidad")
     definicion.configure(text=var_mru[0]['defi_velocidad'])
     xI_lbl = Label(ventana, text= "Ingrese posicion inicial en m:", fg="#000", bg="#FFEBC9")
@@ -92,17 +104,20 @@ def velocidad():
     tF_lbl = Label(ventana, text= "Ingrese tiempo final en s :", fg="#000", bg="#FFEBC9")
     tI_lbl.place(x=44, y=433)
     tF_lbl.place(x=44, y=454)
-    xI_box = Entry(ventana)
+
+    # validacion de texto
+    xI_box = Entry(ventana, validate='key', validatecommand=(validacion, '%P'))
     xI_box.place(x=208, y=391)
-    xF_box = Entry(ventana)
+    xF_box = Entry(ventana, validate='key', validatecommand=(validacion, '%P'))
     xF_box.place(x=208, y=412)
-    tI_box = Entry(ventana)
+    tI_box = Entry(ventana, validate='key', validatecommand=(validacion, '%P'))
     tI_box.place(x=208, y=433)
-    tF_box = Entry(ventana)
+    tF_box = Entry(ventana, validate='key', validatecommand=(validacion, '%P'))
     tF_box.place(x=208, y=454)
     ventana.mainloop()
 
-def tiempo():
+# INGRESAR DATOS TIEMPO
+def entry_tiempo():
     global x_box, v_box
     fondo_btn("tiempo")
     definicion.configure(text=var_mru[0]['defi_tiempo'])
@@ -111,13 +126,14 @@ def tiempo():
     x.place(x=44, y=391)
     v.place(x=44, y=412)
 
-    x_box = Entry(ventana)
+    x_box = Entry(ventana, validate='key', validatecommand=(validacion, '%P'))
     x_box.place(x=208, y=391)
-    v_box = Entry(ventana)
+    v_box = Entry(ventana, validate='key', validatecommand=(validacion, '%P'))
     v_box.place(x=208, y=412)
     ventana.mainloop()
 
-def pos():
+# CALCULO DE POSICION
+def calculo_pos():
     global v, t, posi
     mas_info("pos")
     v = float(v_box.get())
@@ -130,7 +146,8 @@ def pos():
     animacion("pos")
     velo_animacion("pos")
 
-def vel():
+# CALCULO DE VELOCIDAD
+def calculo_vel():
     global xI, xF, tI, tF, div
     mas_info("vel")
     xI = float(xI_box.get())
@@ -145,7 +162,8 @@ def vel():
     animacion("vel")
     velo_animacion("vel")
 
-def tie():
+# CALCULO DE TIEMPO
+def calculo_tie():
     global x, v, tiem
     mas_info("tie")
     x = float(x_box.get())
@@ -158,6 +176,7 @@ def tie():
     animacion("tie")
     velo_animacion("tie")
 
+# BOTON MAS INFO
 def mas_info(dato):
     # BOTONES GRAFICOS
     boton = dato
@@ -205,6 +224,7 @@ def info(dato, boton):
 
             calculo_body = Label(ventana, text=f'\nPara calcular la posición usaremos los datos anteriormente dados, elegidos por usted.\nLo que haremos sera multiplicar la velocidad constante por el tiempo total.\n\n    {v} * {tiem} = {x}\n\nCon esto listo podemos conocer que la incognita de la posición final es {x}m.\n', bg='#DFDDC7', justify=LEFT, anchor='w')
             calculo_body.place(x=3, y=343, width=463)
+        # CARGAR IMAGENES EN TKINTER
         img = PhotoImage(file="img\F03.png") 
         img_x = Label(ventana, image=img)
         img_x.place(x=239, y=239)
@@ -217,25 +237,32 @@ def info(dato, boton):
             datos = Label(ventana, text=f'\n Posición inicial: 0m\n\n Posición final: {posi}m\n\n Tiempo inicial: 0s\n\n Tiempo final: {t}s\n\n Velocidad: ???m/s\n', bg='#DFDDC7', justify=LEFT, anchor='w')
             datos.place(x=3, y=200, width=463)
 
-            calculo_body = Label(ventana, text=f'\nPara calcular la velocidad constante usaremos los datos anteriormente dados, \nelegidos por usted.\nLo que haremos sera dividir la posición final con el tiempo total.\n\n   {posi} / {t} = {v}\n\nCon esto realizado podremos conocer que la \nincognita de la velocidad constante es de {v}m/s\n', justify=LEFT, anchor='w')
+            calculo_body = Label(ventana, text=f'\nPara calcular la velocidad constante usaremos los datos anteriormente dados, \nelegidos por usted.\nLo que haremos sera dividir la posición final con el tiempo total.\n\n   {posi} / {t} = {v}\n\nCon esto realizado podremos conocer que la \nincognita de la velocidad constante es de {v}m/s\n', bg='#DFDDC7', justify=LEFT, anchor='w')
             calculo_body.place(x=3, y=403, width=463)
         elif boton == "vel":
             datos = Label(ventana, text=f'\n Posición inicial: {xI}m\n\n Posición final: {xF}m\n\n Tiempo inicial: {tI}s\n\n Tiempo final: {tF}s\n\n Velocidad: ???m/s\n', bg='#DFDDC7', justify=LEFT, anchor='w')
             datos.place(x=3, y=200, width=463)
 
-            calculo_body = Label(ventana, text=f'\nPara calcular la velocidad constante usaremos los datos anteriormente dados, \nelegidos por usted.\nLo que haremos sera dividir la posición final con el tiempo total.\n\n   {xF} / {tF} = {div}\n\nCon esto realizado podremos conocer que la \nincognita de la velocidad constante es de {v}m/s\n', justify=LEFT, anchor='w')
+            calculo_body = Label(ventana, text=f'\nPara calcular la velocidad constante usaremos los datos anteriormente dados, \nelegidos por usted.\nLo que haremos sera dividir la posición final con el tiempo total.\n\n   {xF} / {tF} = {div}\n\nCon esto realizado podremos conocer que la \nincognita de la velocidad constante es de {v}m/s\n', bg='#DFDDC7', justify=LEFT, anchor='w')
             calculo_body.place(x=3, y=403, width=463)
         elif boton == "tie":
             datos = Label(ventana, text=f'\n Posición inicial: 0m\n\n Posición final: {x}m\n\n Tiempo inicial: 0s\n\n Tiempo final: {tiem}s\n\n Velocidad: ???m/s\n', bg='#DFDDC7', justify=LEFT, anchor='w')
             datos.place(x=3, y=200, width=463)
 
-            calculo_body = Label(ventana, text=f'\nPara calcular la velocidad constante usaremos los datos anteriormente dados, \nelegidos por usted.\nLo que haremos sera dividir la posición final con el tiempo total.\n\n   {x} / {tiem} = {v}\n\nCon esto realizado podremos conocer que la \nincognita de la velocidad constante es de {v}m/s\n', justify=LEFT, anchor='w')
+            calculo_body = Label(ventana, text=f'\nPara calcular la velocidad constante usaremos los datos anteriormente dados, \nelegidos por usted.\nLo que haremos sera dividir la posición final con el tiempo total.\n\n   {x} / {tiem} = {v}\n\nCon esto realizado podremos conocer que la \nincognita de la velocidad constante es de {v}m/s\n', bg='#DFDDC7', justify=LEFT, anchor='w')
             calculo_body.place(x=3, y=403, width=463)
+
+        # CARGAR IMAGENES EN TKINTER
         img = PhotoImage(file="img\F04.png")
         img_v = Label(ventana, image=img)
         img_v.place(x=239, y=260)
     ventana.mainloop()
 
+def ingreso_texto(text):
+    if text.count(',') <= 1 and all(char.isdigit() or char == ',' for char in text) or text == "":
+        return True
+    else:
+        return False
 # VENTANA
 ventana = tk.Tk()
 ventana.title('Movimiento Rectilíneo Uniforme (MRU)')
@@ -243,8 +270,10 @@ ventana.geometry('1280x700')
 ventana.resizable(False, False)
 ventana.configure(bg='#DFDDC7') 
 
+validacion = ventana.register(ingreso_texto)
+
 # TEXTOS
-with open('textos.json', 'r', encoding='utf-8') as textos:
+with open('json/textos.json', 'r', encoding='utf-8') as textos:
     contenido_mru = json.load(textos)
 # Definicion general
 var_mru = contenido_mru
@@ -256,6 +285,7 @@ definicion = Label(ventana, text=var_mru[0]['def_mru'], bg='#C38154', fg='#fff',
 pregunta = Label(ventana, text='¿Qué desea calcular?', bg='#211717', fg='#fff')
 split = Frame(ventana, bg='#211717')
 
+# UBICACION MAIN BACKGROUND
 body.place(x=5, y=5, width=366, height=690)
 header.place(x=5, y=5, width=366, height=42)
 definicion.place(x = 38, y = 57, width= 300, height= 140)
@@ -264,10 +294,11 @@ split.place(x=376, y=0, width=5, height=700)
 
 # BOTONES MAIN
 btn_bg = Frame(ventana, bg='#C38154')
-btn_pos = Button(ventana, text='Posicion recorrida', bg='#DFDDC7', command=posicion)
-btn_v = Button(ventana, text='Velocidad constante', bg='#DFDDC7', command=velocidad)
-btn_t = Button(ventana, text='Tiempo total', bg='#DFDDC7', command=tiempo)
+btn_pos = Button(ventana, text='Posicion recorrida', bg='#DFDDC7', command=entry_posicion)
+btn_v = Button(ventana, text='Velocidad constante', bg='#DFDDC7', command=entry_velocidad)
+btn_t = Button(ventana, text='Tiempo total', bg='#DFDDC7', command=entry_tiempo)
 
+# UBICACION BOTONES MAIN
 btn_bg.place(x=15, y=255, width=346, height=430)
 btn_pos.place(x=25, y=265, width=326, height=25)
 btn_v.place(x=25, y=295, width=326, height=25)
@@ -277,10 +308,11 @@ btn_t.place(x=25, y=325, width=326, height=25)
 header_animado = Frame(bg='#A34A28')
 graph_body1 = Frame(ventana, bg='#A34A28')
 graph_body2 = Frame(ventana, bg='#A34A28')
-graph_datos1 = Label(ventana, text=var_mru[1]['defi_grafico_posicion'], bg='#C38154')
-graph_datos2 = Label(ventana, text=var_mru[1]['defi_grafico_velocidad'], bg='#C38154')
+graph_datos1 = Label(ventana, text=var_mru[1]['defi_grafico_posicion'], bg='#C38154', fg='#fff')
+graph_datos2 = Label(ventana, text=var_mru[1]['defi_grafico_velocidad'], bg='#C38154', fg='#fff')
 vel_anima = Label(ventana, text='INGRESA DATOS',bg='#DFDDC7', fg='#000')
 
+# UBICACION MAIN GRAFICOS / DATOS
 header_animado.place(x=386, y=5, width=888, height=150)
 graph_body1.place(x=386, y=171, width=442, height=524)
 graph_body2.place(x=833, y=171, width=442, height=524)
@@ -298,18 +330,18 @@ def graficos(dato):
         ax1.plot([0, t], [0, posi])
         ax2.plot([0, t], [v,v])
     elif dato == "vel":
-        ax1.plot([tI, tF], [xI, xF])
+        ax1.plot(s[tI, tF], [xI, xF])
         ax2.plot([tI, tF], [div,div])
     elif dato == "tie":
         ax1.plot([0, tiem], [0, x])
         ax2.plot([0, tiem], [v, v])
     ax1.set_title('Grafico Posición - Tiempo')
-    ax1.set(xlabel='T (s)', ylabel='X (m)')
+    ax1.set(xlabel='Tiempo (s)', ylabel='Posición (m)')
     ax1.grid(color='black', linestyle='--', linewidth=0.5)
     fig1.tight_layout()
 
     ax2.set_title('Grafico Velocidad - Tiempo')
-    ax2.set(xlabel='T (s))', ylabel='V (m/s)')
+    ax2.set(xlabel='Tiempo (s))', ylabel='Velocidad (m/s)')
     ax2.grid(color='black', linestyle='--', linewidth=0.5)
     fig2.tight_layout()
 
